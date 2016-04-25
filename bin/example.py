405a4b4776be6df76aa4d5e91e319efef60e8ae7
Kaelan Cooter <dcgamers@gmail.com>
2016-04-25 19:49:27 -0400

    changed some settings
    Former-commit-id: f038090dd95d2d5381b9cbdf71af58bbe7975380 [formerly f038090dd95d2d5381b9cbdf71af58bbe7975380 [formerly 630ecee077d91a12feee779165e7c25c41e15dc4]]
    Former-commit-id: dc8f39fc7c3a5e4486b6f2e2cbb76a8e393c2def
    Former-commit-id: a066186313c3a6e7e027b43ff3194607a33a4e3e

--- bin/example.py
+++ bin/example.py
@@ -3,11 +3,13 @@ import json
 from historia import Historia
 
 if __name__ == "__main__":
-    file_path = '/Users/kaelan/www/hexgen/bin/export.json'
+    file_path = os.path.join(os.path.expanduser('~'), 'hexgen.json')
     output_path = os.path.join(os.path.expanduser('~'), 'historia.json')
     print('Output: {}'.format(output_path))
 
     with open(file_path) as fobj:
-        gen = Historia(json.load(fobj), debug=False)
+        gen = Historia(json.load(fobj), debug=False, params={
+            'run_days': 100
+        })
         gen.start()
         gen.export(output_path)
