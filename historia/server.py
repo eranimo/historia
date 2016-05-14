a1d3948adc8f8529a5f3a6217f01693213c82cdc
Kaelan Cooter <dcgamers@gmail.com>
2016-05-14 17:41:11 -0400

    switched to just flask, debug parameter on historia command
    Former-commit-id: c4ce91b0de46a664154fa1cad838aea7b4042a1d

--- historia/server.py
+++ historia/server.py
@@ -1,25 +1,19 @@
-import socketio
-import eventlet
 from flask import Flask, render_template
+from historia.gen import Historia
 
-sio = socketio.Server()
-
-@sio.on('connect', namespace='/history')
-def connect(sid, environ):
-    print("connect ", sid)
-
-@sio.on('history message', namespace='/history')
-def message(sid, data):
-    print("message ", data)
-    sio.emit(sid, 'reply')
-
-@sio.on('disconnect', namespace='/history')
-def disconnect(sid):
-    print('disconnect ', sid)
-
-
-def start_server(port=8888):
+def start_server(port=8888, debug=False):
     print("Starting Historia server on port {}".format(port))
+
+    world = Historia.from_data(debug)
+
     app = Flask(__name__)
-    app = socketio.Middleware(sio, app)
-    eventlet.wsgi.server(eventlet.listen(('', port)), app)
+
+    @app.route('/world_data')
+    def world_data():
+        return flask.jsonify(**world.world_data())
+
+    @app.route('/next_day')
+    def next_day():
+        return flask.jsonify(**world.next_day())
+
+    app.run()
